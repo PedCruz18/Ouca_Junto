fb7e9e77d4f696ebc54ce829188a51f5b125b8f5
PedCruz <Pedroemanueldasilvacruz@gmail.com>
2025-04-15 22:06:24 -0300

    teste

--- static/js/SistemaAudio.js
+++ static/js/SistemaAudio.js
@@ -23,37 +23,37 @@ export const socket = io(URL_SERVIDOR, {
 
 export const logger = {
  log: (...args) => {
-  if (!emProducao) {
+  if (emProducao) {
    console.log(...args);
   }
  },
  warn: (...args) => {
-  if (!emProducao) {
+  if (emProducao) {
    console.warn(...args);
   }
  },
  error: (...args) => {
-  if (!emProducao) {
+  if (emProducao) {
    console.error(...args);
   }
  },
  info: (...args) => {
-  if (!emProducao) {
+  if (emProducao) {
    console.info(...args);
   }
  },
  debug: (...args) => {
-  if (!emProducao) {
+  if (emProducao) {
    console.debug(...args);
   }
  },
  groupCollapsed: (...args) => {
-  if (!emProducao) {
+  if (emProducao) {
     console.debug(...args);
   }
  },
  groupEnd: (...args) => {
-  if (!emProducao) {
+  if (emProducao) {
    console.debug(...args);
   }
  },
