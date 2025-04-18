54b4c633d69e1896417439b8f55131263b3c8cc3
PedCruz <Pedroemanueldasilvacruz@gmail.com>
2025-04-18 19:19:41 -0300

    V0.0.57 - Estável

--- modules/routes.py
+++ modules/routes.py
@@ -2,5 +2,13 @@ from flask import render_template
 
 def init_routes(app):
     @app.route('/')
-    def InterfaceTransmissao():
+    def inicio():
+        return render_template('inicio.html')
+    
+    @app.route('/radio')
+    def radio():
         return render_template('radio.html')
+    
+    @app.route('/sobre')
+    def sobre():
+        return render_template('sobre.html')
